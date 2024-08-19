57c27409e6abaf5adbfb3a2f72148d07df45407a
James Chang <jamesbchang@gmail.com>
2024-08-19 11:09:22 +0900

    update

--- listing-item-app/pages/index.tsx
+++ listing-item-app/pages/index.tsx
@@ -3,7 +3,10 @@ import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1 className="bg-gray-200">Hello Next.js 👋</h1>
+    <div className="bg-gray-400 text-blue-200">
+      This is a group testing..
+    </div>
     <p>
       <Link href="/about">About</Link>
     </p>
